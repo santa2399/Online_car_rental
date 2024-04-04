af8ec99a82ff6e57b26a99cc5252fe2ec42da4d4
santa2399 <124044017+santa2399@users.noreply.github.com>
2024-04-04 12:13:56 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 // DB credentials.
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','');
+define('DB_PASS','sangeetha@12345');
 define('DB_NAME','carrental');
 // Establish database connection.
 try
