c43063ad2c0fac1326683c19e7d06877f9e0601d
santa2399 <124044017+santa2399@users.noreply.github.com>
2024-04-04 12:13:31 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,16 @@
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:carrentaldb1.database.windows.net,1433; Database = carrental", "carrental", "{sangeetha@12345}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+<?php 
+// DB credentials.
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','carrental');
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
 }
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "carrental", "pwd" => "{sangeetha@12345}", "Database" => "carrental", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:carrentaldb1.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
