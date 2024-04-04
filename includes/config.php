c304283eaf1df3256860ff27fba31c4f815076a0
santa2399 <124044017+santa2399@users.noreply.github.com>
2024-04-04 12:10:56 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:carrentaldb1.database.windows.net,1433; Database = carrental", "carrental", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:carrentaldb1.database.windows.net,1433; Database = carrental", "carrental", "{sangeetha@12345}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "carrental", "pwd" => "{your_password_here}", "Database" => "carrental", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "carrental", "pwd" => "{sangeetha@12345}", "Database" => "carrental", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:carrentaldb1.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
