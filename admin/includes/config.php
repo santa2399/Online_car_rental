674147e8f47c4ed1a6484a523ed3520453741937
santa2399 <124044017+santa2399@users.noreply.github.com>
2024-04-05 10:57:32 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,16 +1,23 @@
+
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','carrental');
+define('DB_HOST','mydbcarrent.mysql.database.azure.com');
+define('DB_USER','sangeetha');
+define('DB_PASS','Azure@123');
+define('DB_NAME','sk');
 // Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
+try {
+    $dbh = new PDO(
+        "mysql:host=".DB_HOST.";dbname=".DB_NAME,
+        DB_USER,
+        DB_PASS,
+        array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+            PDO::MYSQL_ATTR_SSL_CA => '/path/to/ca-cert.pem', // Path to CA certificate
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false // Set to false if server certificate verification is not needed
+        )
+    );
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
 }
 ?>
